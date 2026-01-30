92d83bdd691e03628293e29bffc930f53088d51a
Logann Leriche <lleriche@c2r12p6.42lehavre.fr>
2026-01-30 17:20:49 +0100

    module3

--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -4,11 +4,14 @@ import math
 if len(sys.argv) == 1:
     print("No scores provided. Usage: "
           "python3 ft_score_analytics.py <score1> <score2> ...")
-
-cord = [-5, 50, 25]
-cord2 = [100, 36, 0]
-cordTuple = tuple(cord)
+# coordone entre par l utilisateur
+nombres = sys.argv[1].split(" ")
+cord2 = [int(n) for n in nombres]
 cordTuple2 = tuple(cord2)
+# coordonne de base
+cord = [0, 0, 0]
+cordTuple = tuple(cord)
+
 # calcul de distance
 distance = math.sqrt((cordTuple2[0]-cordTuple2[0])**2 +
                      (cordTuple2[1]-cordTuple[1])**2 +
